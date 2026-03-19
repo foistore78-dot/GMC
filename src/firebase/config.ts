3be3eee88b89be3259c902330583266af00abf40
Salvatore Fois <fois.tore78@gmail.com>
2026-03-19 08:50:07 +0000

    ti ho perso

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9577324505-15044",
-  "appId": "1:889295517264:web:438d49e4da66ede530c355",
-  "apiKey": "AIzaSyAJ9MA-QXL0Qcxz3GSl3wC_XYDzhH_DJ60",
+  "apiKey": "AIzaSyAcldSN-fuMTx3ImCAzVCH_kkoIBUNAeoY",
   "authDomain": "studio-9577324505-15044.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "889295517264"
+  "projectId": "studio-9577324505-15044",
+  "storageBucket": "studio-9577324505-15044.firebasestorage.app",
+  "messagingSenderId": "889295517264",
+  "appId": "1:889295517264:web:f2a1934b70728e0c30c355"
 };
